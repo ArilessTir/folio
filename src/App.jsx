a0c1278d0f42cc5affdf6d5dfc40443708ac1176
ArilessTir <ariless.tir@gmail.com>
2022-07-25 20:52:11 +0200

    update desc

--- src/App.jsx
+++ src/App.jsx
@@ -60,15 +60,15 @@ function App() {
         <p className="about__desc">
           My journey in computer science and data started in 2015, where I
           studied statistics and computer science at l'Université de Lille. In
-          parallel, I worked two years in work-study contracts for La Poste as a
-          Data Analyst. Then I continued my education in master computer science
-          at L'Université Catholique Lille, with a contract in work-study
-          contracts at Cerfrance as a Data Scientist, where I could build and
-          deploy machine learning projects. Today I work for CGI as a Data
-          Engineer for a retail client, near Lille. <br /> <br />I am passionate
-          about Machine Learning and Mlops technics, and i am looking for
-          missions or job in those areas. So if you think we can match feel free
-          to contact me. 😀
+          parallel, I worked two years with a work-study contracts at La Poste
+          as a Data Analyst. Then I continued my education in master computer
+          science at L'Université Catholique Lille, with a work-study contracts
+          at Cerfrance as a Data Scientist, where I could build and deploy
+          machine learning projects. Today I work for CGI as a Data Engineer for
+          a retail client, near Lille. <br /> <br />I am passionate about
+          Machine Learning and Mlops technics, and i am looking for missions or
+          job in those areas. So if you think we can match feel free to contact
+          me. 😀
         </p>
       </section>
 
